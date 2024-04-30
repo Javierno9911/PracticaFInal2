185f47644885d04ca07e618eb662e6947e9c9fa4
Javierno9911 <47241759+Javierno9911@users.noreply.github.com>
2024-04-30 11:33:27 +0200

    v 0.0.9
    Delete cerrau y retoques en el test

--- routers/publicaciones.js
+++ routers/publicaciones.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const { getPaginasWebComercio, crearPaginaWebComercio, obtenerPaginaWebComercioPorId, actualizarPaginaWebComercio, eliminarPaginaWebComercio  } = require("../controllers/publicacionController");
-const { validatorCreateAdmin, validatorGetAdmin } = require("../validators/publicacion")
+const { validatorCreateItem, validatorGetItem } = require("../validators/publicacion")
 
 // Ruta para obtener todas las páginas web de comercios
-router.get("/",getPaginasWebComercio);
-
-// Ruta para crear una nueva página web de comercio
-router.post("/",crearPaginaWebComercio);
+router.get("/", getPaginasWebComercio);
 
 // Ruta para obtener una página web de comercio por su ID
-router.get("/:id",obtenerPaginaWebComercioPorId);
+router.get("/:id", validatorGetItem , obtenerPaginaWebComercioPorId);
+
+// Ruta para crear una nueva página web de comercio
+router.post("/", validatorCreateItem, crearPaginaWebComercio);
 
 // Ruta para actualizar una página web de comercio por su ID
-router.put("/:id",actualizarPaginaWebComercio);
+router.put("/:id", validatorGetItem, actualizarPaginaWebComercio);
 
 // Ruta para eliminar una página web de comercio
-router.delete("/:id",eliminarPaginaWebComercio);
+router.delete("/:id", validatorGetItem, eliminarPaginaWebComercio);
 
 module.exports = router;
--- routers/usuarios.js
+++ routers/usuarios.js
@@ -3,52 +3,13 @@ const router = express.Router();
 const { getUsuarios, crearUsuario, obtenerUsuarioPorId, actualizarUsuario, eliminarUsuario} = require("../controllers/usuarioController");
 const { validatorCreateUser, validatorGetUser } = require("../validators/usuario")
 
-/**
- * @openapi
- * /api/usuario/tablaDeUsuario:
- *  get:
- *      tags:
- *      - User
- *      summary: Get users in the System
- *      responses:
- *          '200':
- *              description: Returns the users
- *          '500':
- *              description: Server error
- *      security:
- *          - bearerAuth: []
- */
+// Ruta para obtener todos los usuarios
 router.get("/", getUsuarios);
 
 // Ruta para obtener un usuario por su ID
 router.get("/:id",validatorGetUser, obtenerUsuarioPorId);
 
-/**
- * @openapi
- * tags:
- *   name: Books
- *   description: The books managing API
- * /books:
- *   post:
- *     summary: Create a new book
- *     tags: [Books]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Book'
- *     responses:
- *       200:
- *         description: The created book.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Book'
- *       500:
- *         description: Some server error
- *
- */
+// Ruta para crear usuarios.
 router.post("/", validatorCreateUser, crearUsuario);
 
 // Ruta para actualizar un usuario por su ID
