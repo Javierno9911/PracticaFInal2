e8261359b12ba2c2c1cf4210185caf22d47b5732
Javierno9911 <47241759+Javierno9911@users.noreply.github.com>
2024-05-09 16:28:25 +0200

    v 1.0.4
    Conectau con slack cachau

--- index.js
+++ index.js
@@ -2,46 +2,31 @@ const express = require("express")
 const cors = require("cors")
 require('dotenv').config();
 const dbConnect = require('./config/mongo')
-//const loggerStream = require("./utils/handleLogger")
-
 const swaggerUi = require("swagger-ui-express")
 const swaggerSpecs = require("./docs/swagger")
-
+const loggerStream = require("./utils/handleLogger")
 const morganBody = require("morgan-body")
-const {IncomingWebhook} = require("@slack/webhook")
-/*
-const webHook = new IncomingWebhook(process.env.SLACK_WEBHOOK)
 
-const loggerStream = {
-    write: message => {
-        webHook.send({text: message})
-    },
-}
+const app = express();
 
 morganBody(app, {
-    noColors: true, //limpiamos el String de datos lo máximo posible antes de mandarlo a Slack
-    skip: function(req, res) { 
-        return res.statusCode < 400
+    noColors: true,
+    skip: function(req, res) {
+    return res.statusCode < 400
     },
     stream: loggerStream
 })
-*/
 
-const app = express()
+
 
 //Le decimos a la app de express() que use cors para evitar el error Cross-Domain (XD)
 app.use(cors())
 app.use(express.json())
 
-console.log("Especificaciones de Swagger:", swaggerSpecs);
+//console.log("Especificaciones de Swagger:", swaggerSpecs);
 
 // Configuración de Swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
-/*
-app.use("/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpecs)
-)*/
 
 app.use("/api", require("./routers"))
 
